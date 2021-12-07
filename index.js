4729d828c3562623555ff7ba8763bd8a4f8c8b7c
Victor Turitzin <turitzinv@gmail.com>
2021-12-06 21:01:42 -0500

    commit tree f4cc5ef01a759fe2dc753e62219ecd706778e062

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching (drivers, name) {
+    return drivers.filter((employee) => employee.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch (drivers, name) {
+    let fullName = drivers.filter((employee) => employee.slice(0, name.length) === name)
+    return fullName
+}
+
+function matchName (drivers, name) {
+    let match = drivers.filter((employee) => employee.name === name)
+    console.log(match)
+    return match
+}
